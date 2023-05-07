1623d6872d334107ded71f5594dc1b7164049c7b
ballin-23 <ballincode@gmail.com>
2023-05-07 14:43:32 -0600

    project setup

---
+++ dividend-calculator/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Calculator/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Calculator from '@/components/Calculator.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Calculator
+  }
+});
+</script>
+
+<style>
+</style>
---
+++ dividend-calculator/src/components/Calculator.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script lang="ts">
+export default {
+name: 'Calculator',
+setup() {
+    // assume dividends are paid monthly for now
+    const calculateDividendReturn = (amountInvested: number, years: number, dividend: number, recurringInvestment: number) => {
+        // get the dividend as a percentage
+        const div = dividend / 100
+        // A = P (1 + r/n)^(nt)
+        const total = amountInvested * (1+(div/12))**(12*years)
+        console.log(total)
+
+
+    }
+    calculateDividendReturn(10000,1, 10, 400)
+}
+}
+</script>
+
+<style>
+
+</style>
